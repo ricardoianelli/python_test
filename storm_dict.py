fa6c8cc7ff9ed0bc6003f3c4535ed5409435cd82
Ricardo Ianelli <ricardoianelli@hotmail.com>
2019-09-12 13:37:05 -0300

    Creating a dictionary version of the challenge

---
+++ storm_dict.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 21:06:36 2019
+
+@author: RICARDO
+"""
+
+# Define base data
+facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('joão', 'endereço', 'rua alice, 10', True),
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '234-5678', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ('joão', 'telefone', '234-5678', False),
+    ('gabriel', 'telefone', '98888-1111', True),
+    ('gabriel', 'telefone', '56789-1010', True),
+]
+
+# Define data cardinality
+schema = [
+    ('endereço', 'cardinality', 'one'),
+    ('telefone', 'cardinality', 'many'),
+]
+
+
+def dict_to_facts(dic):
+    fact_list = []
+    for entity, fact in dic.items():
+        for attribute, values in fact.items():
+            for value in values:
+                fact_list.append((entity, attribute, value, True))
+    return fact_list
+    
+def get_current_facts(facts, schema):
+    many_cardionality = {attr[0]: (attr[2] == 'many') for attr in schema}
+    clean_facts = {}
+    
+    for fact in facts:
+        entity = clean_facts.get(fact[0], {})
+        attr = entity.get(fact[1], [])
+        if many_cardionality[fact[1]]:
+            if fact[3]:
+                attr.append(fact[2])
+            else:
+                attr.remove(fact[2])
+        else:
+            if fact[3]:
+                if len(attr) > 0:
+                    attr[0] = fact[2]
+                else:
+                    attr.append(fact[2])
+            else:
+                attr.remove(fact[2])
+                
+        entity[fact[1]] = attr
+        clean_facts[fact[0]] = entity
+    
+    clean_facts = dict_to_facts(clean_facts)
+    clean_facts.sort()
+    return clean_facts
+     
+current_facts = get_current_facts(facts, schema)
---
+++ test_storm_dict.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 13:17:59 2019
+
+@author: RICARDO
+"""
+
+
+import pytest
+import storm_dict as storm
+
+def test_expected_get_current_facts_1():
+  expected = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '56789-1010', True),  
+    ('gabriel', 'telefone', '98888-1111', True), 
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ]
+  actual = storm.get_current_facts(storm.facts, storm.schema)
+  message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+  # Complete the assert statement
+  assert actual == expected, message
+  
+def test_expected_get_current_facts_2():
+    facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('joão', 'endereço', 'rua alice, 10', True),
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '234-5678', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ('joão', 'telefone', '234-5678', False),
+    ('gabriel', 'telefone', '98888-1111', True),
+    ('gabriel', 'telefone', '56789-1010', True),
+    ('gabriel', 'endereço', 'av rio branco, 109', False),
+    ]
+    
+    expected = [
+    ('gabriel', 'telefone', '56789-1010', True),  
+    ('gabriel', 'telefone', '98888-1111', True), 
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ]
+    actual = storm.get_current_facts(facts, storm.schema)
+    message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+    # Complete the assert statement
+    assert actual == expected, message
+  
+def test_expected_get_current_facts_3():
+    facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('joão', 'endereço', 'rua alice, 10', True),
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '234-5678', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ('joão', 'telefone', '234-5678', False),
+    ('gabriel', 'telefone', '98888-1111', True),
+    ('gabriel', 'telefone', '56789-1010', True),
+    ('gabriel', 'telefone', '56789-1010', False),
+    ('gabriel', 'endereço', 'av rio branco, 109', False),
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ]
+    
+    expected = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '98888-1111', True), 
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '91234-5555', True),
+    ]
+    actual = storm.get_current_facts(facts, storm.schema)
+    message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+    # Complete the assert statement
+    assert actual == expected, message
+  
+def test_get_current_facts_format_1():
+  facts = [
+    ('gabriel', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '56789-1010', True),  
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '234-5678', True),
+    ]
+  with pytest.raises(KeyError):
+      storm.get_current_facts(facts, storm.schema)
+      
+def test_get_current_facts_format_2():
+  facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '56789-1010'),  
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', '234-5678', True),
+    ]
+  with pytest.raises(IndexError):
+      storm.get_current_facts(facts, storm.schema)
+      
+def test_get_current_facts_format_3():
+  facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '56789-1010', True),  
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('joão', 'telefone', True),
+    ]
+  with pytest.raises(IndexError):
+      storm.get_current_facts(facts, storm.schema)
+      
+def test_get_current_facts_format_4():
+  facts = [
+    ('gabriel', 'endereço', 'av rio branco, 109', True),
+    ('gabriel', 'telefone', '56789-1010', True),  
+    ('joão', 'endereço', 'rua bob, 88', True),
+    ('telefone', '234-5678', True),
+    ]
+  with pytest.raises(KeyError):
+      storm.get_current_facts(facts, storm.schema)
+  
